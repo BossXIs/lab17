207886b3e41640c469822ef1204db80a2fdaabf9
BossXI <bossweerawut@hotmail.com>
2022-01-18 22:04:27 +0700

    lab17_1

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,43 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double *data,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(data+i);
+        if((i+1)%M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *data,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(data+i) = (rand()%101)/100.0;
+    }
+}
+
+
+void findRowSum(const double *dPtr,double *sum1,int N,int M){
+ int count = 0;
+ double sum = 0;
+ for (int i  = 0; i < M*N; i++){
+  sum += dPtr[i];
+  if((i+1)%M == 0){
+      sum1[count] = sum;
+      sum = 0;
+      count++;
+        }
+ }
+}
+
+void findColSum(const double *dPtr,double *sum2,int N,int M){
+    for (int j = 0; j < M ; j++) {
+		sum2[j] = 0;
+	}
+ 	for (int i  = 0; i < M*N; i++){
+  		sum2[(i)%M] += dPtr[i];
+ 	}
+}
+
+
